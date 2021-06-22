8fd6497358158e4e476e842ceff418d741bd3231
Shishir Kumar <shishir.insane@gmail.com>
2021-06-22 10:21:19 +0200

    added kubernetes dashboad management scripts and config

--- script/dashboard.sh
+++ script/dashboard.sh
@@ -7,6 +7,10 @@ kubectl apply -f ../../svetlana/kubernetes/dashboard/recommended.yaml
 echo  "Token: "
 kubectl -n kube-system describe secret default
 
+# chmod +x ~/dashboard/dashboard.sh
+# sudo ln -s ~/dashboard/dashboard.sh /usr/local/bin/dashboard
+# dashboard start|status|stop
+
 echo "Starting the dashboard..."
 echo "Dashboard is accessible at: "
 echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/"
